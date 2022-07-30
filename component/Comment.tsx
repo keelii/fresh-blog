62dff919ebc68701f41796242c2f940156c49723
keelii <keeliizhou@gmail.com>
2022-07-31 07:37:00 +0800

    feat: 使用 giscus 评论

--- component/Comment.tsx
+++ component/Comment.tsx
@@ -4,17 +4,33 @@ import { h } from "preact";
 export function Comment(props: any) {
   return (
     <div className="comment">
-      <script
-        src="https://utteranc.es/client.js"
-        // @ts-ignore:
-        repo="keelii/fresh-blog"
-        issue-term="title"
-        label="评论"
-        theme="github-light"
+      <script src="https://giscus.app/client.js"
+        data-repo="keelii/fresh-blog"
+        data-repo-id="R_kgDOHmFUTA"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOHmFUTM4CQh7f"
+        data-mapping="title"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="zh-CN"
+        data-loading="lazy"
         crossOrigin="anonymous"
-        async
-      >
+        async>
       </script>
+      {/*<script*/}
+      {/*  src="https://utteranc.es/client.js"*/}
+      {/*  // @ts-ignore:*/}
+      {/*  repo="keelii/fresh-blog"*/}
+      {/*  issue-term="title"*/}
+      {/*  label="评论"*/}
+      {/*  theme="github-light"*/}
+      {/*  crossOrigin="anonymous"*/}
+      {/*  async*/}
+      {/*>*/}
+      {/*</script>*/}
     </div>
   );
 }
