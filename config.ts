496b492c7b348d6a40c4f42b9d8d4845ebf3c844
keelii <keeliizhou@gmail.com>
2025-09-02 08:10:08 +0800

    feat: add about redirect.

--- config.ts
+++ config.ts
@@ -20,6 +20,7 @@ export const REDIRECTS = {
   "/2019/07/03/nestjs-framework-tutorial-8": "/2019/07/04/nestjs-framework-tutorial-8",
   "/2020/05/10/frontend-dev-bottleneck-and-future": "/2020/05/11/frontend-dev-bottleneck-and-future",
   "/2020/08/14/take-a-look-at-deno-from-actual-case": "/2020/08/15/take-a-look-at-deno-from-actual-case"
+  "/2016/06/13/about": "/about"
 }
 
 warn("CONFIG:" + JSON.stringify({
