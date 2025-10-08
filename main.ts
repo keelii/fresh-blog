776befd02a6d77053f42403c4bcb8ab8356cd23a
keelii <keeliizhou@gmail.com>
2025-10-08 18:49:11 +0800

    fix: to throw Error

--- main.ts
+++ main.ts
@@ -8,8 +8,7 @@ import writeTextFile = Deno.writeTextFile
 async function fetchRemote() {
   const BLOG_API_URI = Deno.env.get("BLOG_API_URI")
   if (!BLOG_API_URI) {
-    console.error("BLOG_API_URI is missing")
-    Deno.exit(1)
+    throw new Error("BLOG_API_URI is missing")
   }
   const res = await fetch(BLOG_API_URI)
   const json = await res.json()
